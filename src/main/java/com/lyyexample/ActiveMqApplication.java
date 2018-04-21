7e0d9ed3268cec2cf19d5e7a77c4bc254dba03ce
yeshen2013 <1486258506@qq.com>
2018-04-21 22:51:55 +0800

    ActiveMQdemo

---
+++ src/main/java/com/lyyexample/ActiveMqApplication.java
@@ -0,0 +1,12 @@
+package com.lyyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveMqApplication.class, args);
+	}
+}
